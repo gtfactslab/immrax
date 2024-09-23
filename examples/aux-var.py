715a61d4a13d689bac251a9f8e5a487cc7273802
Brendan Gould <brendan.gould314@gmail.com>
2024-09-23 17:40:37 -0400

    Independent LPs for lower / upper

--- examples/aux-var.py
+++ examples/aux-var.py
@@ -1,24 +1,28 @@
+from typing import List
+
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import numpy as onp
+from pypoman import plot_polygon
+from scipy.spatial import HalfspaceIntersection
+
 import immrax as irx
 from immrax.embedding import AuxVarEmbedding, TransformEmbedding
-from immrax.utils import draw_iarray
-from immrax.inclusion import interval
-import jax.numpy as jnp
-import numpy as onp
-from scipy.spatial import HalfspaceIntersection
-import matplotlib.pyplot as plt
-from pypoman import plot_polygon
+from immrax.inclusion import Interval, interval, natif
+from immrax.utils import draw_iarray, linprog_refine, run_times
 
-from immrax.utils import linprog_refine, run_times
-from immrax.inclusion import natif
-# Code up gradient descent steps in dual var 
+# Code up gradient descent steps in dual var
 # Read papers about contraction and stability
 
-A: jnp.array = jnp.array([[0.0, -1.0], [1.0, 0.0]]) # row major
+A: jax.Array = jnp.array([[0.0, -1.0], [1.0, 0.0]])  # row major
+sim_len = 1.56
+
 
 # TODO: I have to do this for now because I can't jit a linear program
-def linprog_traj(A, x0, H, t0, tf, dt = 0.01):
+def linprog_traj(A, x0, H, t0, tf, dt=0.01):
     sim_len = int((tf - t0) / dt)
-    bounds = [None] * sim_len
+    bounds: List[None | Interval] = [None] * sim_len
     bounds[0] = x0
     Hp = jnp.linalg.pinv(H)
 
@@ -29,24 +33,26 @@ def linprog_traj(A, x0, H, t0, tf, dt = 0.01):
 
         Fkwargs = lambda x: emb_upd(linprog_refine(H)(x))
 
-        n = H.shape[0] 
+        n = H.shape[0]
         _x = x.lower
         x_ = x.upper
 
-    # Computing F on the faces of the hyperrectangle
+        # Computing F on the faces of the hyperrectangle
+        # interval(_x, x_.at[i].set(_x))
+
         _X = interval(
-        jnp.tile(_x, (n, 1)), jnp.where(jnp.eye(n), _x, jnp.tile(x_, (n, 1)))
-    )
-        _E_lower = [None] * len(_X)
+            jnp.tile(_x, (n, 1)), jnp.where(jnp.eye(n), _x, jnp.tile(x_, (n, 1)))
+        )
+        _E_lower: List[None | jax.Array] = [None] * len(_X)
         for i in range(len(_X)):
             fx = Fkwargs(_X[i])
             _E_lower[i] = fx.lower
         _E = irx.interval(_E_lower, _E_lower)
 
         X_ = interval(
-        jnp.where(jnp.eye(n), x_, jnp.tile(_x, (n, 1))), jnp.tile(x_, (n, 1))
-    )
-        E__upper = [None] * len(_X)
+            jnp.where(jnp.eye(n), x_, jnp.tile(_x, (n, 1))), jnp.tile(x_, (n, 1))
+        )
+        E__upper: List[None | jax.Array] = [None] * len(_X)
         for i in range(len(X_)):
             fx = Fkwargs(X_[i])
             E__upper[i] = fx.upper
@@ -55,9 +61,10 @@ def linprog_traj(A, x0, H, t0, tf, dt = 0.01):
         return irx.interval(jnp.diag(_E.lower), jnp.diag(E_.upper))
 
     for i in range(1, sim_len):
-        bounds[i] = bounds[i-1] + interval(dt) * update(bounds[i-1])
-        
+        bounds[i] = bounds[i - 1] + interval(dt) * update(bounds[i - 1])
+
     return bounds
+
 
 class HarmOsc(irx.System):
     def __init__(self) -> None:
@@ -68,10 +75,16 @@ class HarmOsc(irx.System):
         x1, x2 = x.ravel()
         return jnp.array([-x2, x1])
 
+
 # Number of subdivisions of [0, pi] to make aux vars for
 # Certain values of this are not good choices, as they will generate angles theta=0 or theta=pi/2
-N = 3
-aux_vars = jnp.array([[jnp.cos(n * jnp.pi / (N+1)), jnp.sin(n * jnp.pi / (N+1))] for n in range(1, N+1)])
+N = 6
+aux_vars = jnp.array(
+    [
+        [jnp.cos(n * jnp.pi / (N + 1)), jnp.sin(n * jnp.pi / (N + 1))]
+        for n in range(1, N + 1)
+    ]
+)
 x0_int = irx.icentpert(jnp.array([1.0, 0.0]), jnp.array([0.1, 0.1]))
 
 # Trajectory of unrefined system
@@ -79,7 +92,7 @@ osc = HarmOsc()
 embsys = TransformEmbedding(osc)
 traj = embsys.compute_trajectory(
     0.0,
-    1.56,
+    sim_len,
     irx.i2ut(x0_int),
 )
 
@@ -88,25 +101,21 @@ tfinite = jnp.where(jnp.isfinite(traj.ts))
 ts_clean = traj.ts[tfinite]
 ys_clean = traj.ys[tfinite]
 
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "Helvetica",
-    "font.size": 14
-})
+plt.rcParams.update({"text.usetex": True, "font.family": "CMU Serif", "font.size": 14})
 plt.figure()
 
 y_int = [irx.ut2i(y) for y in ys_clean]
 for timestep, bound in zip(ts_clean, y_int):
     draw_iarray(plt.gca(), bound, alpha=0.4)
-plt.gcf().suptitle("No aux vars")
+plt.gcf().suptitle("Harmonic Oscillator with Uncertainty (No Refinement)")
 
 # Plot refined trajectories
 fig, axs = plt.subplots(int(jnp.ceil(N / 3)), 3, figsize=(5, 5))
-fig.suptitle("Harmonic Oscillator with Uncertainty")
+fig.suptitle("Harmonic Oscillator with Uncertainty (Sampled Refinement)")
 axs = axs.reshape(-1)
 
 fig_lp, axs_lp = plt.subplots(int(jnp.ceil(N / 3)), 3, figsize=(5, 5))
-fig_lp.suptitle("Harmonic Oscillator with Uncertainty (LP)")
+fig_lp.suptitle("Harmonic Oscillator with Uncertainty (LP Refinement)")
 axs_lp = axs_lp.reshape(-1)
 
 H = jnp.array([[1.0, 0.0], [0.0, 1.0]])
@@ -117,37 +126,50 @@ for i in range(len(aux_vars)):
     lifted_x0_int = interval(H) @ x0_int
 
     # Compute new refined trajectory
-    auxsys = AuxVarEmbedding(osc, H, num_samples=10**(i+1))
-    traj, time = run_times(1, auxsys.compute_trajectory,
+    auxsys = AuxVarEmbedding(osc, H, num_samples=10 ** (i + 1))
+    traj, time = run_times(
+        1,
+        auxsys.compute_trajectory,
         0.0,
-        1.56,
+        sim_len,
         irx.i2ut(lifted_x0_int),
+        solver="euler",
     )
     tfinite = jnp.where(jnp.isfinite(traj.ts))
     ts_clean = traj.ts[tfinite]
     ys_clean = traj.ys[tfinite]
     print(f"\tSample for {i+1} aux vars took: {time}")
+    print(f"\tFinal bound: \n{irx.ut2i(ys_clean[-1])[:2]}")
 
-    lp_traj, time = run_times(1, linprog_traj, A, lifted_x0_int, H, 0.0, 1.56)
+    lp_traj, time = run_times(1, linprog_traj, A, lifted_x0_int, H, 0.0, sim_len)
     print(f"\tLinprog for {i+1} aux vars took: {time}")
+    print(f"\tFinal bound: \n{lp_traj[-1][:2]}")
 
     # Clean up and display results
     y_int = [irx.ut2i(y) for y in ys_clean]
     plt.sca(axs[i])
     axs[i].set_title(rf"$\theta = {i+1} \frac{{\pi}}{{{N+1}}}$")
     for timestep, bound in zip(ts_clean, y_int):
-        cons = onp.hstack((onp.vstack((-H, H)), onp.concatenate((bound.lower, -bound.upper)).reshape(-1, 1)))
+        cons = onp.hstack(
+            (
+                onp.vstack((-H, H)),
+                onp.concatenate((bound.lower, -bound.upper)).reshape(-1, 1),
+            )
+        )
         hs = HalfspaceIntersection(cons, bound.center[0:2])
         vertices = hs.intersections
 
         plot_polygon(vertices, fill=False, resize=True, color="tab:blue")
 
-    # print(f"\tFinal bound:\n{irx.ut2i(ys_clean[-1])[:2]}")
-
     plt.sca(axs_lp[i])
     axs_lp[i].set_title(rf"$\theta = {i+1} \frac{{\pi}}{{{N+1}}}$")
     for bound in lp_traj:
-        cons = onp.hstack((onp.vstack((-H, H)), onp.concatenate((bound.lower, -bound.upper)).reshape(-1, 1)))
+        cons = onp.hstack(
+            (
+                onp.vstack((-H, H)),
+                onp.concatenate((bound.lower, -bound.upper)).reshape(-1, 1),
+            )
+        )
         hs = HalfspaceIntersection(cons, bound.center[0:2])
         vertices = hs.intersections
 
--- immrax/utils.py
+++ immrax/utils.py
@@ -1,150 +1,203 @@
-import jax
-import jax.numpy as jnp
-import numpy as onp
+from math import exp, floor, log
 import time
-from jax._src.util import wraps
+from typing import Callable, Tuple
+
+import jax
 from jax._src.traceback_util import api_boundary
-from immrax.inclusion import Interval, Corner, all_corners
-from immrax.inclusion import i2ut, i2lu, i2centpert, ut2i, icentpert, icopy, interval
-from typing import Callable, List, Tuple
-import shapely.geometry as sg
-import shapely.ops as so
+from jax._src.util import wraps
+import jax.numpy as jnp
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as onp
 import scipy.optimize as opt
-from math import floor, exp, log
-from functools import partial
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import shapely.geometry as sg
+import shapely.ops as so
 
-def timed (f:Callable) :
+from immrax.inclusion import (
+    Corner,
+    Interval,
+    all_corners,
+    i2lu,
+    i2ut,
+    icopy,
+    interval,
+    ut2i,
+)
+
+
+def timed(f: Callable):
     @wraps(f)
     @api_boundary
-    def f_timed (*args, **kwargs) :
+    def f_timed(*args, **kwargs):
         t0 = time.time()
         ret = jax.block_until_ready(f(*args, **kwargs))
         tf = time.time()
         return ret, (tf - t0)
+
     return f_timed
 
-def run_times (N:int, f:Callable, *args, **kwargs) :
+
+def run_times(N: int, f: Callable, *args, **kwargs):
     f_timed = timed(f)
     times = []
-    for i in range(N) :
+    for i in range(N):
         ret, dt = f_timed(*args, **kwargs)
         times.append(dt)
     return ret, jnp.array(times)
 
-def d_metzler (A) :
+
+def d_metzler(A):
     diag = jnp.diag_indices_from(A)
     Am = jnp.clip(A, 0, jnp.inf).at[diag].set(A[diag])
     return Am, A - Am
 
-def d_positive (B) :
+
+def d_positive(B):
     return jnp.clip(B, 0, jnp.inf), jnp.clip(B, -jnp.inf, 0)
 
-sg_box = lambda x, xi=0, yi=1 : sg.box(x[xi].lower,x[yi].lower,x[xi].upper,x[yi].upper)
-sg_boxes = lambda xx, xi=0, yi=1 : [sg_box(x, xi, yi) for x in xx]
 
-def draw_sg_union (ax, boxes, **kwargs) :
+sg_box = lambda x, xi=0, yi=1: sg.box(
+    x[xi].lower, x[yi].lower, x[xi].upper, x[yi].upper
+)
+sg_boxes = lambda xx, xi=0, yi=1: [sg_box(x, xi, yi) for x in xx]
+
+
+def draw_sg_union(ax, boxes, **kwargs):
     shape = so.unary_union(boxes)
     xs, ys = shape.exterior.xy
-    kwargs.setdefault('ec', 'tab:blue')
-    kwargs.setdefault('fc', 'none')
-    kwargs.setdefault('lw', 2)
-    kwargs.setdefault('alpha', 1)
+    kwargs.setdefault("ec", "tab:blue")
+    kwargs.setdefault("fc", "none")
+    kwargs.setdefault("lw", 2)
+    kwargs.setdefault("alpha", 1)
     ax.fill(xs, ys, **kwargs)
 
-draw_iarray = lambda ax, x, xi=0, yi=1, **kwargs : draw_sg_union(ax, [sg_box(x, xi, yi)], **kwargs)
-draw_iarrays = lambda ax, xx, xi=0, yi=1, **kwargs: draw_sg_union(ax, sg_boxes(xx, xi, yi), **kwargs)
 
-def draw_iarray_3d (ax, x, xi=0, yi=1, zi=2, **kwargs) :
-    Xl, Yl, Zl = x.lower[(xi,yi,zi),]
-    Xu, Yu, Zu = x.upper[(xi,yi,zi),]
-    poly_alpha = kwargs.pop('poly_alpha', 0.)
-    kwargs.setdefault('color', 'tab:blue')
-    kwargs.setdefault('lw', 0.75)
-    faces = [ \
-        onp.array([[Xl,Yl,Zl],[Xu,Yl,Zl],[Xu,Yu,Zl],[Xl,Yu,Zl],[Xl,Yl,Zl]]), \
-        onp.array([[Xl,Yl,Zu],[Xu,Yl,Zu],[Xu,Yu,Zu],[Xl,Yu,Zu],[Xl,Yl,Zu]]), \
-        onp.array([[Xl,Yl,Zl],[Xu,Yl,Zl],[Xu,Yl,Zu],[Xl,Yl,Zu],[Xl,Yl,Zl]]), \
-        onp.array([[Xl,Yu,Zl],[Xu,Yu,Zl],[Xu,Yu,Zu],[Xl,Yu,Zu],[Xl,Yu,Zl]]), \
-        onp.array([[Xl,Yl,Zl],[Xl,Yu,Zl],[Xl,Yu,Zu],[Xl,Yl,Zu],[Xl,Yl,Zl]]), \
-        onp.array([[Xu,Yl,Zl],[Xu,Yu,Zl],[Xu,Yu,Zu],[Xu,Yl,Zu],[Xu,Yl,Zl]]) ]
-    for face in faces :
-        ax.plot3D(face[:,0], face[:,1], face[:,2], **kwargs)
-        kwargs['alpha'] = poly_alpha
+draw_iarray = lambda ax, x, xi=0, yi=1, **kwargs: draw_sg_union(
+    ax, [sg_box(x, xi, yi)], **kwargs
+)
+draw_iarrays = lambda ax, xx, xi=0, yi=1, **kwargs: draw_sg_union(
+    ax, sg_boxes(xx, xi, yi), **kwargs
+)
+
+
+def draw_iarray_3d(ax, x, xi=0, yi=1, zi=2, **kwargs):
+    Xl, Yl, Zl = x.lower[(xi, yi, zi),]
+    Xu, Yu, Zu = x.upper[(xi, yi, zi),]
+    poly_alpha = kwargs.pop("poly_alpha", 0.0)
+    kwargs.setdefault("color", "tab:blue")
+    kwargs.setdefault("lw", 0.75)
+    faces = [
+        onp.array(
+            [[Xl, Yl, Zl], [Xu, Yl, Zl], [Xu, Yu, Zl], [Xl, Yu, Zl], [Xl, Yl, Zl]]
+        ),
+        onp.array(
+            [[Xl, Yl, Zu], [Xu, Yl, Zu], [Xu, Yu, Zu], [Xl, Yu, Zu], [Xl, Yl, Zu]]
+        ),
+        onp.array(
+            [[Xl, Yl, Zl], [Xu, Yl, Zl], [Xu, Yl, Zu], [Xl, Yl, Zu], [Xl, Yl, Zl]]
+        ),
+        onp.array(
+            [[Xl, Yu, Zl], [Xu, Yu, Zl], [Xu, Yu, Zu], [Xl, Yu, Zu], [Xl, Yu, Zl]]
+        ),
+        onp.array(
+            [[Xl, Yl, Zl], [Xl, Yu, Zl], [Xl, Yu, Zu], [Xl, Yl, Zu], [Xl, Yl, Zl]]
+        ),
+        onp.array(
+            [[Xu, Yl, Zl], [Xu, Yu, Zl], [Xu, Yu, Zu], [Xu, Yl, Zu], [Xu, Yl, Zl]]
+        ),
+    ]
+    for face in faces:
+        ax.plot3D(face[:, 0], face[:, 1], face[:, 2], **kwargs)
+        kwargs["alpha"] = poly_alpha
         ax.add_collection3d(Poly3DCollection([face], **kwargs))
 
-def draw_iarrays_3d (ax, xx, xi=0, yi=1, zi=2, color='tab:blue') :
-    for x in xx :
+
+def draw_iarrays_3d(ax, xx, xi=0, yi=1, zi=2, color="tab:blue"):
+    for x in xx:
         draw_iarray_3d(ax, x, xi, yi, zi, color)
 
-def plot_interval_t (ax, tt, x, **kwargs) :
+
+def plot_interval_t(ax, tt, x, **kwargs):
     xl, xu = i2lu(x)
-    alpha = kwargs.pop('alpha', 0.25)
-    label = kwargs.pop('label', None)
+    alpha = kwargs.pop("alpha", 0.25)
+    label = kwargs.pop("label", None)
     ax.fill_between(tt, xl, xu, alpha=alpha, label=label, **kwargs)
     ax.plot(tt, xl, **kwargs)
     ax.plot(tt, xu, **kwargs)
 
 
-def get_half_intervals (x:Interval, N=1, ut=False) :
+def get_half_intervals(x: Interval, N=1, ut=False):
     _xx_0 = i2ut(x) if ut is False else x
     n = len(_xx_0) // 2
     ret = [_xx_0]
-    for i in range (N) :
+    for i in range(N):
         newret = []
-        for _xx_ in ret :
-            cent = (_xx_[:n] + _xx_[n:])/2
-            for part_i in range(2**n) :
+        for _xx_ in ret:
+            cent = (_xx_[:n] + _xx_[n:]) / 2
+            for part_i in range(2**n):
                 part = jnp.copy(_xx_)
-                for ind in range (n) :
-                    part = part.at[ind + n*((part_i >> ind) % 2)].set(cent[ind])
+                for ind in range(n):
+                    part = part.at[ind + n * ((part_i >> ind) % 2)].set(cent[ind])
                 newret.append(part)
         ret = newret
-    if ut :
+    if ut:
         return ret
-    else :
+    else:
         return [ut2i(part) for part in ret]
 
+
 # @partial(jax.jit,static_argnums=(1,))
-def get_partitions_ut (x:jax.Array, N:int) -> jax.Array :
+def get_partitions_ut(x: jax.Array, N: int) -> jax.Array:
     n = len(x) // 2
     # c^n = N
-    c = floor(exp(log(N)/n) + 1e-10)
-    _x = x[:n]; x_ = x[n:]
+    c = floor(exp(log(N) / n) + 1e-10)
+    _x = x[:n]
+    x_ = x[n:]
     xc = []
-    for i in range(c+1) :
-        xc.append(_x + i*(x_ - _x)/c)
+    for i in range(c + 1):
+        xc.append(_x + i * (x_ - _x) / c)
     l = onp.arange(c)
-    A = onp.array(onp.meshgrid(*[l for i in range(n)])).reshape((n,-1)).T
+    A = onp.array(onp.meshgrid(*[l for i in range(n)])).reshape((n, -1)).T
     ret = []
-    for i in range(len(A)) :
-        _part = jnp.array([xc[A[i,j]][j] for j in range(n)])
-        part_ = jnp.array([xc[A[i,j]+1][j] for j in range(n)])
-        ret.append(jnp.concatenate((_part,part_)))
+    for i in range(len(A)):
+        _part = jnp.array([xc[A[i, j]][j] for j in range(n)])
+        part_ = jnp.array([xc[A[i, j] + 1][j] for j in range(n)])
+        ret.append(jnp.concatenate((_part, part_)))
     return jnp.array(ret)
 
-def gen_ics (x0, N, key=jax.random.key(0)) :
+
+def gen_ics(x0, N, key=jax.random.key(0)):
     # X = np.empty((N, len(x0)))
     X = []
     keys = jax.random.split(key, len(x0))
-    for i in range(len(x0)) :
+    for i in range(len(x0)):
         # X[:,i] = uniform_disjoint(range, N)
-        X.append(jax.random.uniform(key=keys[i],shape=(N,),minval=x0.lower[i], maxval=x0.upper[i]))
+        X.append(
+            jax.random.uniform(
+                key=keys[i], shape=(N,), minval=x0.lower[i], maxval=x0.upper[i]
+            )
+        )
     return jnp.array(X).T
 
-def set_columns_from_corner(corner:Corner, A:Interval):
+
+def set_columns_from_corner(corner: Corner, A: Interval):
     _Jx = jnp.where(jnp.asarray(corner) == 0, A.lower, A.upper)
     J_x = jnp.where(jnp.asarray(corner) == 0, A.upper, A.lower)
     return _Jx, J_x
 
-def get_corners (x:Interval, corners:Tuple[Corner]|None=None) :
+
+def get_corners(x: Interval, corners: Tuple[Corner] | None = None):
     corners = all_corners(len(x)) if corners is None else corners
     xut = i2ut(x)
-    return jnp.array([jnp.array([x.lower[i] if c[i] == 0 else x.upper[i] for i in range(len(x))]) for c in corners])
+    return jnp.array(
+        [
+            jnp.array([x.lower[i] if c[i] == 0 else x.upper[i] for i in range(len(x))])
+            for c in corners
+        ]
+    )
 
-def I_refine (A:jax.Array) -> Callable[[Interval], Interval]:
-    def vec_refine(null_vector: jax.Array, var_index: int, y:Interval):
+
+def I_refine(A: jax.Array) -> Callable[[Interval], Interval]:
+    def vec_refine(null_vector: jax.Array, var_index: int, y: Interval):
         ret = icopy(y)
 
         # Set up linear algebra computations for the refinement
@@ -153,9 +206,9 @@ def I_refine (A:jax.Array) -> Callable[[Interval], Interval]:
         b1 = lambda: ((-bounding_vars @ ret) / ref_var) & ret[var_index]
         b2 = lambda: ret[var_index]
 
-        # Compute refinement based on null vector, if possible 
-        ndb0 = (jnp.abs(null_vector[var_index]) > 1e-10)
-        ret = jax.lax.cond(ndb0, b1, b2) 
+        # Compute refinement based on null vector, if possible
+        ndb0 = jnp.abs(null_vector[var_index]) > 1e-10
+        ret = jax.lax.cond(ndb0, b1, b2)
 
         # fix fpe problem with upper < lower
         retu = jnp.where(ret.upper >= ret.lower, ret.upper, ret.lower)
@@ -164,58 +217,51 @@ def I_refine (A:jax.Array) -> Callable[[Interval], Interval]:
     mat_refine = jax.vmap(vec_refine, in_axes=(0, None, None), out_axes=0)
     mat_refine_all = jax.vmap(mat_refine, in_axes=(None, 0, None), out_axes=1)
 
-    def best_refinement(y:Interval):
+    def best_refinement(y: Interval):
         refinements = mat_refine_all(A, jnp.arange(len(y)), y)
-        return interval(jnp.max(refinements.lower, axis=0), jnp.min(refinements.upper, axis=0))
-    
+        return interval(
+            jnp.max(refinements.lower, axis=0), jnp.min(refinements.upper, axis=0)
+        )
+
     return best_refinement
 
+
 def linprog_refine(H: jax.Array) -> Callable[[Interval], Interval]:
-    def I_r(y: Interval):
+    def I_r(y: Interval) -> Interval:
         ret = icopy(y)
         n = len(ret)
-        # min sum_{j \ne i} |a_j| (y_j.upper - y_j.lower) s.t. a_i = 1 and a @ H = 0
-        # We do this by adding synthetic variables a_j', and requiring that a_j <= a_j' and -a_j <= a_j'
-        obj_vec = jnp.array(
-            [(ret[i].upper - ret[i].lower) for i in range(n)]
-        )
-        obj_vec = jnp.append(jnp.zeros_like(obj_vec), obj_vec)
-        A_eq = jnp.kron(jnp.array([1, 0]), H.T) 
-        A_eq = jnp.vstack((A_eq, jnp.zeros_like(A_eq[0])))
-        b_eq = jnp.zeros(A_eq.shape[0]).at[-1].set(1) # Need H.T @ a = 0, a_i = 1
-        A_ub = jnp.kron(jnp.array([[1, -1], [-1, -1]]), jnp.eye(n))
-        b_ub = jnp.zeros_like(obj_vec) # Need a_j - a_j' <= 0, -a_j - a_j' <= 0
+        A_ub = jnp.vstack((H, -H))
 
-        # Choose the best vector in the null space to bound each variable
         for i in range(n):
-            obj_vec_i = obj_vec.at[i + n].set(0)
-            A_eq_i = A_eq.at[-1, i].set(1)
+            obj_vec_i = jnp.zeros(n).at[i].set(1) @ H
+            b_ub = jnp.concatenate((ret.upper, -ret.lower))
 
-            sol = opt.linprog(
+            sol_min = opt.linprog(
                 c=obj_vec_i,
-                A_eq=A_eq_i,
-                b_eq=b_eq,
                 A_ub=A_ub,
                 b_ub=b_ub,
-                bounds=(None, None)
+                bounds=(None, None),
+            )
+            sol_max = opt.linprog(
+                c=-obj_vec_i,
+                A_ub=A_ub,
+                b_ub=b_ub,
+                bounds=(None, None),
             )
 
             # If a vector that gives extra info on this var is found, refine bounds
-            if sol.success:
-                null_vec = interval(sol.x[:n])
-                reti = (
-                    (-null_vec[:i] @ ret[:i] - null_vec[i + 1 :] @ ret[i + 1 :])
-                ) & ret[i]
+            if sol_min.success:
+                retl = ret.lower.at[i].set(jnp.maximum(sol_min.fun, ret.lower[i]))
+                ret = interval(retl, ret.upper)
 
-                retl = ret.lower.at[i].set(reti.lower)
-                retu = ret.upper.at[i].set(
-                    jnp.where(reti.upper >= reti.lower, reti.upper, reti.lower)
-                )
-                ret = interval(retl, retu)
+            if sol_max.success:
+                retu = ret.upper.at[i].set(jnp.minimum(-sol_max.fun, ret.upper[i]))
+                ret = interval(ret.lower, retu)
 
         return ret
 
     return I_r
+
 
 def null_space(A, rcond=None):
     """Taken from scipy, with some modifications to use jax.numpy"""
@@ -225,5 +271,5 @@ def null_space(A, rcond=None):
         rcond = jnp.finfo(s.dtype).eps * max(M, N)
     tol = jnp.amax(s) * rcond
     num = jnp.sum(s > tol, dtype=int)
-    Q = vh[num:,:].T.conj()
+    Q = vh[num:, :].T.conj()
     return Q
