afa95429cd8cfb06c505f103890f28e8b63c6d6c
Brendan Gould <brendan.gould314@gmail.com>
2025-05-16 11:17:50 -0400

    feat: final touches on plots

--- examples/auxillary_vars/aux-var.py
+++ examples/auxillary_vars/aux-var.py
@@ -1,10 +1,13 @@
 import pickle
-from typing import Literal
+from typing import Literal, Tuple
 import time
 
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+from pyparsing import line
 
 import immrax as irx
 from immrax.embedding import AuxVarEmbedding
@@ -15,7 +18,9 @@ from immrax.utils import (
     run_times,
     draw_trajectory_2d,
     draw_refined_trajectory_2d,
+    gen_ics,
 )
+
 
 # Read papers about contraction and stability
 
@@ -43,7 +48,9 @@ class VanDerPolOsc(irx.System):
         return jnp.array([self.mu * (x1 - 1 / 3 * x1**3 - x2), x1 / self.mu])
 
 
-def angular_refined_trajectory(num_aux_vars: int, mode: Literal["sample", "linprog"], save: bool = False) -> Trajectory:
+def angular_refined_trajectory(
+    num_aux_vars: int, mode: Literal["sample", "linprog"], save: bool = False
+) -> Tuple[Trajectory, jax.Array]:
     # Generate angular sweep aux vars
     # Odd num_aux_var is not a good choice, as it will generate angle theta=pi/2, which is redundant with the actual state vars
     aux_vars = angular_sweep(num_aux_vars)
@@ -62,7 +69,7 @@ def angular_refined_trajectory(num_aux_vars: int, mode: Literal["sample", "linpr
     print(f"Compilation took: {time.time() - start:.4g}s")
     print("Compiled.\nComputing trajectory...")
     traj, comp_time = run_times(
-        10,
+        1,
         get_traj,
         0.0,
         sim_len,
@@ -74,13 +81,21 @@ def angular_refined_trajectory(num_aux_vars: int, mode: Literal["sample", "linpr
 
     ys_int = [irx.ut2i(y) for y in traj.ys]
     final_bound = ys_int[-1][2:]
-    final_bound_size = (final_bound[0].upper - final_bound[0].lower) * (final_bound[1].upper - final_bound[1].lower)
+    final_bound_size = (final_bound[0].upper - final_bound[0].lower) * (
+        final_bound[1].upper - final_bound[1].lower
+    )
     print(f"Final bound: \n{final_bound}, size: {final_bound_size}")
 
     if save:
         pickle.dump(ys_int, open(f"{mode}_traj_{num_aux_vars}.pkl", "wb"))
 
-    return traj
+    mc_x0s = gen_ics(x0_int, 30)
+    mc_traj = jax.vmap(
+        lambda x0: sys.compute_trajectory(0, sim_len, x0, solver="euler"),
+    )(mc_x0s)
+
+    return traj, H, mc_traj
+
 
 def plot_angular_refined_trajectory(traj: Trajectory, H: jax.Array):
     fig = plt.figure()
@@ -91,7 +106,6 @@ def plot_angular_refined_trajectory(traj: Trajectory, H: jax.Array):
     # axs = axs.reshape(-1)
 
     draw_refined_trajectory_2d(traj, H)
-    plt.show()
 
 
 x0_int = irx.icentpert(jnp.array([1.0, 0.0]), jnp.array([0.1, 0.1]))
@@ -113,7 +127,21 @@ plt.gcf().suptitle(f"{sys.name} with Uncertainty (No Refinement)")
 draw_trajectory_2d(traj)
 
 
-for i in range(2, 13, 2):
-    traj_s = angular_refined_trajectory(i, "sample")
-    if i <= 6:
-        traj_lp = angular_refined_trajectory(i, "linprog")
+for i in range(6, 7, 2):
+    # traj_s = angular_refined_trajectory(i, "sample")
+    traj_lp, H, mc_traj = angular_refined_trajectory(i, "linprog")
+    plot_angular_refined_trajectory(traj_lp, H)
+    # fig = plt.gcf()
+    x = mc_traj.ys[:, :, 0].T
+    y = mc_traj.ys[:, :, 1].T
+    plt.plot(x, y, alpha=0.5, color="gray", linewidth=0.5)
+
+blue_rectangle = mpatches.Patch(
+    edgecolor="tab:blue", facecolor="none", alpha=0.4, label="Reachable Set Bounds"
+)
+gray_line = mlines.Line2D(
+    [], [], color="gray", alpha=0.5, label="Monte Carlo Trajectories"
+)
+plt.legend(handles=[blue_rectangle, gray_line], loc="lower left")
+
+plt.show()
