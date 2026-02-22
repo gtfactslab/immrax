4b9532014addb080910439ca9e66a4498dce6fab
Akash Harapanahalli <akash.harapanahalli@gmail.com>
2026-02-21 23:08:00 -0500

    recompiled docs and added a docs README

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,20 +11,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-from mock import Mock as MagicMock
-
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-MOCK_MODULES = []
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 project = "immrax"
-copyright = "2023, Akash Harapanahalli"
+copyright = "2026, Akash Harapanahalli"
 author = "Akash Harapanahalli"
 
 # -- General configuration ---------------------------------------------------
